8f0c88cfc1f2097614a2447c73bade9703c2490f
Christiano0407 <placebo0704@gmail.com>
2020-12-21 08:48:51 -0600

    video-2

--- src/js/videoApp.js
+++ src/js/videoApp.js
@@ -1,5 +1,37 @@
 const button = document.getElementById(`playButton`);
+const video = document.querySelector(`video`);
 
-button.addEventListener(`click`, function(){
-    console.log(`Click`);
-});
+//PRIMERA SOLUCIÓN:
+/* button.addEventListener(`click`, function(){
+console.log(`Click`);
+}); */
+function MediaPlayer(){}
+MediaPlayer.prototype.play = function(){
+    video.play();
+}
+const player = new MediaPlayer();
+button.onclick = () => player.play();
+/* button.onclick = () => video.play(); */
+
+//SEGUNDA SOLUCIÓN:
+/*  button.addEventListener(`click`, handlePlayButton, false);
+
+playVideo();
+
+async function playVideo(){
+    try {
+        await video.play();
+        button.className = "playing";
+    }catch(err){
+        button.className ="";
+    }
+}
+
+function handlePlayButton(){
+    if(video.paused){
+        playVideo();
+    }else {
+        video.pause();
+        button.className = "";
+    }
+} */
