f7f921da5bd184220619da51466f004910dd5081
Christiano0407 <placebo0704@gmail.com>
2020-12-22 08:27:06 -0600

    plugins-mute

--- src/js/mediaPlayer.js
+++ src/js/mediaPlayer.js
@@ -4,13 +4,43 @@ console.log(`Click`);
 }); */
  function MediaPlayer(config){
     this.media = config.el;
+    this.plugins = config.plugins || [];
+
+    //método de inicializar:
+    this._initPlugins();
     }
-     MediaPlayer.prototype.play = function(){
+    MediaPlayer.prototype._initPlugins = function(){
+        this.plugins.forEach(plugin => {
+            plugin.run(this)
+        });
+    }
+              //PLAY
+    MediaPlayer.prototype.play = function(){
         if(this.media.paused){
             this.media.play();
         }else {
             this.media.pause();
         }
     }
+              //MUTE
+    MediaPlayer.prototype.mute = function(){
+        this.media.muted = true;
+    }
+     MediaPlayer.prototype.unmute = function(){
+        this.media.muted = false;
+    } 
+    MediaPlayer.prototype.toggleMute = function(){
+        this.media.muted 
+        ? this.unmute()
+        : this.mute();
+    }
 
+              //otro ejemplo:
+ /*    MediaPlayer.prototype.unmuteMute = function(){
+      if(this.media.muted){
+          this.media.muted = false
+      }else {
+          this.media.muted = true;
+      }
+    } */
 export default MediaPlayer;
--- src/js/videoApp.js
+++ src/js/videoApp.js
@@ -1,10 +1,16 @@
 import MediaPlayer from "./mediaPlayer.js";
+import AutoPlay from "../plugins/autoPlay.js"
 
 const button = document.getElementById(`playButton`);
 const video = document.querySelector(`video`);
+const buttonMute = document.querySelector(`#unmuteMute`);
 
-const player = new MediaPlayer({ el:video });
+const player = new MediaPlayer({ el:video, plugins:[
+    new AutoPlay()
+]});
 button.onclick = () => player.play();
+/* buttonMute.onclick = () => player.unmuteMute(); */
+buttonMute.onclick = () => player.toggleMute();
 /* button.onclick = () => video.play(); */
 
 //SEGUNDA SOLUCIÓN:
